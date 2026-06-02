913f4a4701debe5c2849c705efc8a7966f06e818
uiop <uiop@wasdhjkl.xyz>
2026-06-02 17:32:00 -0400

    feat: flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { self, nixpkgs, systems }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      devShells = eachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ racket ];
+            shellHook = ''
+              export PLTADDONDIR="$PWD/.racket"
+              raco pkg install --auto --skip-installed --no-docs html-template
+            '';
+          };
+        });
+    };
+}
