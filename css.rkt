0749ad306ca033ad93b07e0de76d50dd9e932b47
uiop <uiop@wasdhjkl.xyz>
2026-06-02 23:46:40 -0400

    style: naming convention and indentation

--- css.rkt
+++ css.rkt
@@ -1,50 +1,47 @@
 #lang racket
 
-(provide *css* *color-bg*) ; color-bg used for theme color meta
+(provide css color-bg) ; color-bg feeds the theme-color meta tag
 
 (require css-expr)
 
-(define *color-bg* '|#000000|)
+(define color-bg   '|#000000|)
 (define color-fg   '|#cecece|)
 (define color-dim  '|#454545|)
 (define color-dark '|#101010|)
-#;#;
-(define color-link color-fg)
-(define color-rule color-dark)
 
-(define font         'ui-monospace)
-(define font-norm    400)
-(define font-bold    600)
-(define font-size    '|1rem|)
-(define font-size-sm '|0.8rem|)
-(define font-size-md '|0.85rem|)
-(define font-size-lg '|1.25rem|)
-(define font-size-xl '|1.5rem|)
+(define font               'ui-monospace)
+(define font-weight-normal 400)
+(define font-weight-bold   600)
+(define font-size          '|1rem|)
+(define font-size-sm       '|0.8rem|)
+(define font-size-md       '|0.85rem|)
+(define font-size-lg       '|1.25rem|)
+(define font-size-xl       '|1.5rem|)
 
 (define line-height     1.6)
 (define line-height-art 0.85)
 (define letter-spacing  '|0.5rem|)
 
-(define space-xxs '|0.1rem|) ; Separator lines
+(define space-xxs '|0.1rem|) ; separator lines
 (define space-xs  '|0.25rem|)
 (define space-sm  '|0.5rem|)
 (define space-md  '|1rem|)
 (define space-lg  '|1.5rem|)
 (define space-xl  '|4rem|)
 
-(define max-width        '|44rem|)
-(define border-thickness '|1px|)
-(define button-width     '|88px|)
-(define button-height    '|31px|)
+(define max-width     '|44rem|)
+(define border-width  '|1px|)
+(define button-width  '|88px|)
+(define button-height '|31px|)
 
-(define *css*
+(define css
   (css-expr->css
     (css-expr
       [* #:box-sizing border-box]
 
       [body #:margin 0
             #:padding (,space-lg ,space-md ,space-xl)
-            #:background ,*color-bg*
+            #:background ,color-bg
             #:color ,color-fg
             #:font-family ,font
             #:font-size ,font-size
@@ -63,17 +60,17 @@
            #:align-items center
            #:margin-bottom ,space-sm
            #:padding-bottom ,space-sm
-           #:border-bottom (,border-thickness solid ,color-dark)]
+           #:border-bottom (,border-width solid ,color-dark)]
       [|nav a| #:color ,color-dim
-            #:font-size ,font-size
-            #:font-weight ,font-bold
-            #:letter-spacing ,letter-spacing]
+               #:font-size ,font-size
+               #:font-weight ,font-weight-bold
+               #:letter-spacing ,letter-spacing]
 
       [|.links a| #:color ,color-dim
-               #:font-size ,font-size
-               #:font-weight ,font-norm]
+                  #:font-size ,font-size
+                  #:font-weight ,font-weight-normal]
 
-      [header #:border-bottom (,border-thickness solid ,color-dark)
+      [header #:border-bottom (,border-width solid ,color-dark)
               #:margin-bottom ,space-md]
 
       [|.art| #:display block
@@ -87,7 +84,7 @@
 
       [h1 #:text-align center
           #:font-size ,font-size-xl
-          #:font-weight ,font-bold
+          #:font-weight ,font-weight-bold
           #:letter-spacing ,letter-spacing]
 
       [small #:margin (,space-xs 0 ,space-xs)
@@ -99,7 +96,7 @@
 
       [h2 #:margin (0 0 ,space-xs)
           #:font-size ,font-size-lg
-          #:font-weight ,font-bold]
+          #:font-weight ,font-weight-bold]
       [|h2::before| #:content "# "]
 
       [p  #:margin (0 0 ,space-md)]
@@ -121,20 +118,20 @@
                   #:gap ,space-sm
                   #:margin-top ,space-md
                   #:padding-top ,space-md
-                  #:border-top (,border-thickness solid ,color-dark)]
+                  #:border-top (,border-width solid ,color-dark)]
       [|.buttons + .buttons| #:border-top none
-                 #:margin-top ,space-sm
-                 #:padding-top ,space-sm]
+                             #:margin-top ,space-sm
+                             #:padding-top ,space-sm]
       [|.buttons img| #:image-rendering pixelated
-                 #:display block
-                 #:width ,button-width
-                 #:height ,button-height
-                 #:border 0]
+                      #:display block
+                      #:width ,button-width
+                      #:height ,button-height
+                      #:border 0]
       [|.buttons a| #:line-height 0]
 
       [footer #:margin-top ,space-md
               #:padding-top ,space-sm
-              #:border-top (,border-thickness solid ,color-dark)
+              #:border-top (,border-width solid ,color-dark)
               #:text-align center
               #:font-size ,font-size-sm
               #:color ,color-dim]
