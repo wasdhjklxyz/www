a82cc24d803393058c9473fdd287fa92a7003bbd
uiop <uiop@wasdhjkl.xyz>
2026-06-02 23:52:47 -0400

    refactor: move html shit

--- build.rkt
+++ build.rkt
@@ -1,24 +1,6 @@
 #lang racket
 
-(require "css.rkt")
-(require html-template)
+(require "css.rkt" "html.rkt")
 
-(define host    "wasdhjkl.xyz")
-(define tagline "kernels / networking / offensive security / nix btw / nvim btw")
-
-(define html-head
-  (html-template
-    (head
-      (meta (@ (charset "utf-8")))
-      (meta (@ (name "viewport")
-               (content "width=device-width, initial-scale=1")))
-      (meta (@ (name "description")
-               (content (% tagline))))
-      (meta (@ (name "theme-color")
-               (content (% (symbol->string color-bg)))))
-      (link (@ (rel "canonical")
-               (href (% (string-append "https://" host)))))
-      (style (% css))
-      (title (% host)))))
-
-(display html-head)
+(display (html-head #:css css
+                    #:theme-color color-bg))
---
+++ html.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(provide html-head)
+
+(require html-template)
+
+(define host    "wasdhjkl.xyz")
+(define tagline "kernels / networking / offensive security / nix btw / nvim btw")
+
+(define (html-head #:css css
+                   #:theme-color theme-color)
+  (html-template
+    (head
+      (meta (@ (charset "utf-8")))
+      (meta (@ (name "viewport")
+               (content "width=device-width, initial-scale=1")))
+      (meta (@ (name "description")
+               (content (% tagline))))
+      (meta (@ (name "theme-color")
+               (content (% (symbol->string theme-color)))))
+      (link (@ (rel "canonical")
+               (href (% (string-append "https://" host)))))
+      (style (% css))
+      (title (% host)))))
